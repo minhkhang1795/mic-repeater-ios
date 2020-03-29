e3d1394ba0196b0038c6a8fc5911e124d6f96762
Khang Vu <vuminhkhang1995@gmail.com>
2020-03-28 20:14:53 -0500

    Initial Commit

---
+++ mic-repeater-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mic-repeater-ios
+//
+//  Created by Khang Vu on 3/28/20.
+//  Copyright © 2020 Khang Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mic-repeater-iosTests/mic_repeater_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  mic_repeater_iosTests.swift
+//  mic-repeater-iosTests
+//
+//  Created by Khang Vu on 3/28/20.
+//  Copyright © 2020 Khang Vu. All rights reserved.
+//
+
+import XCTest
+@testable import mic_repeater_ios
+
+class mic_repeater_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
