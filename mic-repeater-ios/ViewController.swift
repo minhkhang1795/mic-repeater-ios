8eee96bd9185c50ec6be552a82ab7521ed0a9c83
Khang Vu <vuminhkhang1995@gmail.com>
2020-03-29 00:07:37 -0500

    Implement basic functionality

--- mic-repeater-ios/ViewController.swift
+++ mic-repeater-ios/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var engine = AVAudioEngine()
+    let player = AVAudioPlayerNode()
+    let audioSession = AVAudioSession.sharedInstance()
+    var isRunning = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        do {
+
+            try audioSession.setCategory(AVAudioSession.Category.playAndRecord, options: [.defaultToSpeaker, .allowBluetooth, .allowBluetoothA2DP])
+            try audioSession.setMode(AVAudioSession.Mode.default)
+            try audioSession.setActive(true)
+        } catch {
+            print(error)
+        }
+
+        let input = engine.inputNode
+
+        engine.attach(player)
+
+        let bus = 0
+        let inputFormat = input.inputFormat(forBus: bus)
+
+        engine.connect(player, to: engine.mainMixerNode, format: inputFormat)
+
+        input.installTap(onBus: bus, bufferSize: 512, format: inputFormat) { (buffer, time) -> Void in
+            self.player.scheduleBuffer(buffer)
+            print(buffer)
+        }
     }
 
-
+    @IBAction func start(_ sender: AnyObject) {
+        isRunning = !isRunning
+        sender.setTitle(isRunning ? "Stop" : "Start", for: .normal)
+        
+        if isRunning {
+            do {
+                try engine.start()
+            } catch {
+                print(error)
+            }
+            player.play()
+        } else {
+            engine.stop()
+            player.stop()
+        }
+        
+    }
 }
 
